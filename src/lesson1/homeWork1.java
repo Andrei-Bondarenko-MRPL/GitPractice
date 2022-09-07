56947b71e723dddc5163071d5d0653a6253c2b58
Andrei_Bondarenko <bondorol007@gmail.com>
2022-09-07 21:17:57 +0300

    дз1

---
+++ src/lesson1/homeWork1.java
@@ -0,0 +1,10 @@
+package lesson1;
+
+public class homeWork1 {
+    public static void main(String[] args) {
+        task1();
+    }
+    private static void task1(){
+        System.out.println("solution of the first problem");
+    }
+}
